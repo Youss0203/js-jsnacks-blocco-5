2534fdf2e15d7aa8db7b2f4cc0bb7e3f88607f2c
Youss0203 <youssefhalibi003@gmail.com>
2023-11-27 11:38:03 +0100

    cerato array di automobili

---
+++ js/script.js
@@ -0,0 +1,61 @@
+/* Crea un array composto da 10 automobili.
+Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione 
+(benzina, diesel, gpl, elettrico, metano).
+
+Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, 
+nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+ 
+Infine stampa separatamente i 3 array. */
+
+const automobili=[
+    {
+        marca:"jaguar",
+        modello:"z-3345",
+        alimentazione:"benzina"
+    },
+    {
+        marca:"mercedes",
+        modello:"a-180",
+        alimentazione:"metano"
+    },
+    {
+        marca:"fiat",
+        modello:"panda",
+        alimentazione:"metano"
+    },
+    {
+        marca:"opel",
+        modello:"corsa",
+        alimentazione:"diesel"
+    },
+    {
+        marca:"bmw",
+        modello:"x-7",
+        alimentazione:"diesel"
+    },
+    {
+        marca:"lamborghini",
+        modello:"urus",
+        alimentazione:"benzina"
+    },
+    {
+        marca:"wolkswagen",
+        modello:"tiguan",
+        alimentazione:"gpl"
+    },
+    {
+        marca:"audi",
+        modello:"q8",
+        alimentazione:"benzina"
+    },
+    {
+        marca:"citroen",
+        modello:"c-3",
+        alimentazione:"gpl"
+    },
+    {
+        marca:"renault",
+        modello:"clio",
+        alimentazione:"metano"
+    },
+]
