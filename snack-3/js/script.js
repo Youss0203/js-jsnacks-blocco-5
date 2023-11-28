c6c1a4224576bd3448a6fd3a6ef3165fe54ab97c
Youss0203 <youssefhalibi003@gmail.com>
2023-11-28 12:11:05 +0100

    second commit

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -7,10 +7,13 @@ un array di tab aperte e un numero che indica l'indice della tab aperta nell'arr
 Il software deve guardare se c'è un social aperto ed eliminarlo dalle tab.
 Nel caso la tab fosse attiva, deve attivare la successiva.*/
 
-{
-    tab= ["Facebook", "GitHub", "Gmail","Instagram","Youtube"],
-    activeTab= 0
-}
-console.log(tab , activeTab)
+
+const browserTab = {
+    tab: ["Facebook", "GitHub", "Gmail"],
+    activeTab: 0
+};
+
+
+
 
 
